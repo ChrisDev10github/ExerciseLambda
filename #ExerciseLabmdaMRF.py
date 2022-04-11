83e5a1de4307212966cdb7a1943799c34ae1b15c
Christopher Ng <CNg@dev-10.com>
2022-04-11 16:16:58 -0500

    update

--- #ExerciseLabmdaMRF.py
+++ #ExerciseLabmdaMRF.py
@@ -4,8 +4,19 @@ from functools import reduce
 #Part 1 open file
 
 
-f=open ('911_Calls_For_Service.csv', 'r') 
+f=open ('911_Calls_For_Service_(Last_30_Days).csv', 'r') 
 dictionaryObject = csv.DictReader(f)
-listOfDictionaries = list(dictionaryObject)
+listDictionaries = list(dictionaryObject)
 f.close()
-print(listOfDictionaries)
+
+#Part 1
+
+filtered = list(filter(lambda x: x["zip_code"] !="0" and x["neighborhood"] !='',listDictionaries))
+filteredtrt = list(filter(lambda x: x["totalresponsetime"] !="0" and x["totalresponsetime"] !='',filtered))        
+
+#Average Total Response Time
+#totalDispatch = reduce(lambda time1, time2: time1 + float(time2["dispatchtime"]), filtered)
+totalResponse= reduce(lambda t1, t2: t1 + float(t2["totalresponsetime"]), filtered, 0)
+#totalTime = reduce(lambda time1, time2: time1 + float(time2["totaltime"]), filtered)
+print(totalResponse)
+
