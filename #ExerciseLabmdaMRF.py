7d9106acf5b962f781cd19e12b21560937d2d95c
Christopher Ng <CNg@dev-10.com>
2022-04-11 21:11:10 -0500

    update

--- #ExerciseLabmdaMRF.py
+++ #ExerciseLabmdaMRF.py
@@ -12,11 +12,31 @@ f.close()
 #Part 1
 
 filtered = list(filter(lambda x: x["zip_code"] !="0" and x["neighborhood"] !='',listDictionaries))
-filteredtrt = list(filter(lambda x: x["totalresponsetime"] !="0" and x["totalresponsetime"] !='',filtered))        
+filteredtrt = list(filter(lambda x: x["totalresponsetime"] !="0" and x["totalresponsetime"] !='',filtered))    
+filteredtdt = list(filter(lambda x: x["dispatchtime"] !="0" and x["dispatchtime"] !='',filteredtrt))    
+filteredlist = list(filter(lambda x: x["totaltime"] !="0" and x["totaltime"] !='',filteredtdt))   
 
 #Average Total Response Time
-#totalDispatch = reduce(lambda time1, time2: time1 + float(time2["dispatchtime"]), filtered)
-totalResponse= reduce(lambda t1, t2: t1 + float(t2["totalresponsetime"]), filtered, 0)
-#totalTime = reduce(lambda time1, time2: time1 + float(time2["totaltime"]), filtered)
-print(totalResponse)
+totalDispatch = reduce(lambda time1, time2: time1 + float(time2["dispatchtime"]), filteredlist,0)
+print(f"Average dispatch time: {totalDispatch / len(filteredlist)}")
+totalResponse= reduce(lambda t1, t2: t1 + float(t2["totalresponsetime"]), filteredlist, 0)
+print(f"Average response time: {totalResponse / len(filteredlist)}")
+totalTime = reduce(lambda time1, time2: time1 + float(time2["totaltime"]), filteredlist,0)
+print(f"Average total time: {totalTime / len(filteredlist)}")
 
+
+
+#Part 2
+neighborhoodnames = []
+for i in filteredlist:
+    if i["neighborhood"] not in neighborhoodnames:
+        neighborhoodnames.append(i["neighborhood"])
+
+
+
+
+
+#part 3
+f = open('911.json', "w")
+json.dump(cleanedList, f)
+f.close()
